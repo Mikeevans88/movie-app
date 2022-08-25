8c4f005b41f4557fdb4fc9913e4ea7ce14fce592
Mike Evans <107162236+Mikeevans88@users.noreply.github.com>
2022-08-25 11:07:49 +1000

    update

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -3,13 +3,11 @@ import React from "react"
 const MovieCard = ({ movie }) => {
     return (<div className="movie">
     <div>
-         <p>{movie.Year}</p>
          <div>
              <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
-         </div>
-         <div>
-             <span>{movie.Type}</span>
              <h3>{movie.Title}</h3>
+         <span>{movie.Type}</span>
+         <p>{movie.Year}</p>  
          </div>
          </div>
      </div>
